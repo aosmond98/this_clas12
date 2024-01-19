31385767d43cfb8baca0515907e7bd76320cc4a8
aosmond98 <aosmond@umich.edu>
2024-01-18 19:57:38 -0500

    10.2gev exp

--- src/exe/clas12_analysis.hpp
+++ src/exe/clas12_analysis.hpp
@@ -31,9 +31,9 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<Histogram>& _hi
 
   // Make a data object which all the branches can be accessed from
   // for sim data use 
-  auto data = std::make_shared<Branches12>(_chain, true);
+  // auto data = std::make_shared<Branches12>(_chain, true);
   // for exp data use 
-  // auto data = std::make_shared<Branches12>(_chain);
+  auto data = std::make_shared<Branches12>(_chain);
 
   // Total number of events "Processed"
   size_t total = 0;
@@ -48,8 +48,8 @@ size_t run(std::shared_ptr<TChain> _chain, const std::shared_ptr<Histogram>& _hi
     if (thread_id == 0 && current_event % 1000 == 0)
       std::cout << "\t" << (100 * current_event / num_of_events) << " %\r" << std::flush;
 
-      // use for gen, comment out for rec?? (or use for both??)
-    if (data->mc_npart() < 1) continue;
+      // use for mc, comment out for exp??
+    // if (data->mc_npart() < 1) continue;
 
     // If we pass electron cuts the event is processed
     total++;
--- src/include/reaction.hpp
+++ src/include/reaction.hpp
@@ -75,10 +75,10 @@ class Reaction {
   Reaction(const std::shared_ptr<Branches12> &data, float beam_energy);
   ~Reaction();
   inline float weight() {
-    return _data->mc_weight();
-    // return 1.0;
+    // return _data->mc_weight();
+    return 1.0;
   }
-  // Check lists when you swich from mc (gen) to exp (rec) or vice-versa
+  // Check lists when you swich from mc to exp or vice-versa
   // 1. inline weight function above
   // 2. clas12_yields: auto data = std::make_shared<Branches12>(_chain, true);  turn off true for data
   // 3. from if (data->mc_npart() < 1) to all particle set up im mc events.
